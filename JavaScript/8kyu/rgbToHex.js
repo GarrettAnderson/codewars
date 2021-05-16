663e876d9fb096e99410e513fc4533b49ae0f798
Garrett Anderson <garrettanderson@Garretts-MacBook-Pro.local>
2021-05-15 23:49:59 -0400

    working on converting rgb to hex. got the tostring method to return the hexadecimal but working on figuring out how to deal with tostring with the number zero. need two zerios in hex

---
+++ JavaScript/8kyu/rgbToHex.js
@@ -0,0 +1,18 @@
+function colorOf(r,g,b) {
+  //coding here
+
+  let red = r.toString(16)
+  let grn = g.toString(16)
+  let blue = b.toString(16)
+  let colors = [red, grn, blue]
+
+//   console.log(red.toString(16))
+//   console.log(grn.toString(16))
+//   console.log(blue.toString(16))
+
+  for (let i = 0; i < colors.length; i++) {
+    if (colors[i] = 0) {
+      console.log('zerooo')
+    }
+  }
+}
