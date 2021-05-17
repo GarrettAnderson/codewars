7f7989ccadfeda76eb0197d5ad4e932ee9c7bdd6
Garrett Anderson <garrettanderson@Garretts-MacBook-Pro.local>
2021-05-17 14:37:30 -0400

    added file for a new kata;

---
+++ JavaScript/8kyu/jumping-dutch-man.js
@@ -0,0 +1,8 @@
+function sc(floor){
+  return ?
+
+    // create a variable to store the string for mr despair
+        // take number from input and if greater than 1, respond
+        // according to instructions: floor - 1 amount of 'Aa~' ...
+
+}
