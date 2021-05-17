cfc69619d62f6a89b5386ba45f9f0f1b3c916c89
Garrett Anderson <garrettanderson@Garretts-MacBook-Pro.local>
2021-05-17 13:17:15 -0400

    updated the funtion. able to add an extra zero if the input is zero in order to create the hex color code. now have to figure out adding a zero in front of single digit integers.

--- JavaScript/8kyu/rgbToHex.js
+++ JavaScript/8kyu/rgbToHex.js
@@ -1,18 +1,26 @@
-function colorOf(r,g,b) {
+function colorOf(r,g,b){
   //coding here
 
   let red = r.toString(16)
   let grn = g.toString(16)
-  let blue = b.toString(16)
+  let blue = 10.toString(16)
   let colors = [red, grn, blue]
 
 //   console.log(red.toString(16))
 //   console.log(grn.toString(16))
 //   console.log(blue.toString(16))
 
-  for (let i = 0; i < colors.length; i++) {
-    if (colors[i] = 0) {
-      console.log('zerooo')
+//     console.log(colors)
+
+  for (let i = 0; i <= colors.length; i++) {
+    if (colors[i] === '0') {
+//       console.log(colors[i] + 0)
+        colors[i] = colors[i] + '0'
+        i--
     }
   }
+
+    console.log(colors)
+
+
 }
