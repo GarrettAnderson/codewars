31e4f6611be96c7c1301c5200e7c2e5ef03d2a77
Garrett Anderson <garrettanderson@Garretts-MacBook-Pro.local>
2021-05-18 09:58:18 -0400

    working on solving this previous kata. crossed out the completed kata on the pages sheet.

--- JavaScript/8kyu/count-positives-sum-negatives.js
+++ JavaScript/8kyu/count-positives-sum-negatives.js
@@ -46,3 +46,28 @@ function countPositivesSumNegatives(nums) {
   return []
 
 }
+
+
+
+
+
+
+// function countPositivesSumNegatives(input) {
+//
+// // if input array is empty or null, return an empty array
+//     if (input === null || input.length === 0) {
+//       return []
+//     }
+//
+//     const positives = input
+//       .filter((number) => Math.sign(number) === 1)
+//       .reduce((a) => a + 1, 0)
+//       console.log(positives)
+//
+//     const negatives = input
+//       .filter((number) => Math.sign(number) === -1)
+//       .reduce((a, b) => a + b, 0)
+//
+//   return [positives, negatives]
+//
+// }
