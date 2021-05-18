f26f5cb6af454a7b3764ba9eb34d2dd796a5e1c1
Garrett Anderson <garrettanderson@Garretts-MacBook-Pro.local>
2021-05-18 12:40:50 -0400

    working on getting the elements in reversed array to be convert from strings to numbers to satisfy the instructions

--- JavaScript/8kyu/Number-to-reversed-array-of-digits.js
+++ JavaScript/8kyu/Number-to-reversed-array-of-digits.js
@@ -5,10 +5,11 @@ function digitize(n) {
   let nums = n.toString().split("")
   let rev = nums.reverse()
   let revNumArr = rev.map((el, i) => {
-    console.log(el, i)
-    newArr.push(el)
+//     console.log(el, i)
+    let parsed = parseInt(el)
+    console.log(parsed)
   })
-  console.log(newArr)
+//   console.log(newArr)
 //   console.log(revNumArr)
 
 }
