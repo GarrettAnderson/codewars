90e4c7b9d48c301a8683a89880336cde495d570a
Garrett Anderson <garrettanderson@Garretts-MacBook-Pro.local>
2021-05-17 23:12:06 -0400

    worked on the conditional statements to solve the test cases. was able to solve one test then 5 tests but was still failing one bc i was not considering if the input value for floor is a negative integer.

--- JavaScript/8kyu/jumping-dutch-man.js
+++ JavaScript/8kyu/jumping-dutch-man.js
@@ -4,16 +4,24 @@ function sc(floor){
     // create a variable to store the string for mr despair
         // take number from input and if greater than 1, respond
         // according to instructions: floor - 1 amount of 'Aa~' ...
-  let scream = 'Aa~ '.repeat(floor - 1)
-  console.log(scream)
 
-  if (floor > 1 || floor <= 6) {
-    console.log(scream + 'Pa! Aa!')
+  while (floor > 0) {
+    let scr = 'Aa~ '
+    let scream = scr.repeat(floor - 1)
+
+//     console.log(scream)
+
+  if (floor > 0 && floor > 1 && floor <= 6) {
+//     console.log(scream + 'Pa! Aa!')
     return scream + 'Pa! Aa!'
-  } else if (floor > 6) {
-    console.log(scream + 'Pa!')
+  } else if (floor >= 7) {
+//     console.log(scream + 'Pa!')
     return scream + 'Pa!'
-  } else {
+  } else if (floor <= 1) {
+    console.log(scream)
     return ''
   }
+}
+  return ''
+//   console.log('testing')
 }
