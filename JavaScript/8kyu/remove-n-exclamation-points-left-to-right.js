6b50880910ee736c7fef517d9f941c587ed0ddf9
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2021-05-27 16:14:43 -0400

    still working on removing n number of exclatmation points

--- JavaScript/8kyu/remove-n-exclamation-points-left-to-right.js
+++ JavaScript/8kyu/remove-n-exclamation-points-left-to-right.js
@@ -6,7 +6,8 @@ function remove(s,n){
   for (let i = 0; i < n; i++) {
     if (splitStr[i] === '!') {
 //       console.log('counting')
-      let removed = splitStr.splice(i, 3)
+      let removed = splitStr.splice(i, n)
+      n--
 //       console.log(removed)
 
       }
