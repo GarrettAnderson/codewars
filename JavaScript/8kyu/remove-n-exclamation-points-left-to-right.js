0e970db25e657ad3b7aa57eb5e140730d884055e
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2021-05-31 01:32:26 -0400

    updating n variable to reduce by 1 for every exclatmition point removed. added console.logs to track the various values of n per round of the loop.

--- JavaScript/8kyu/remove-n-exclamation-points-left-to-right.js
+++ JavaScript/8kyu/remove-n-exclamation-points-left-to-right.js
@@ -4,11 +4,12 @@ function remove(s,n){
   let splitStr = s.split('')
 
   for (let i = 0; i < n; i++) {
+    console.log(n)
     if (splitStr[i] === '!') {
 //       console.log('counting')
       let removed = splitStr.splice(i, 1, '')
       n -= 1
-//       console.log(removed)
+      console.log(n)
       }
 //     else {
 //         i--
