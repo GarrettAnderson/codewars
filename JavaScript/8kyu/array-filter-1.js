570aa66b10ee3465ae8bf1d66d821606895a6cfc
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2021-06-09 03:07:41 -0400

    created a new file for a kata for implementing the array filter method

---
+++ JavaScript/8kyu/array-filter-1.js
@@ -0,0 +1,24 @@
+Array.prototype.filter = function (callBackFunction) {
+  //nothing here, you will feel pretty awesome to write down by yourself
+  //have fun
+
+  //PEDAC:
+  // 1. Loop over the array of numbers
+       // being assesed as less than or greater than 3
+  // 2. If the current value is greater than 3, store in a new array
+  // 3. If the current value is less than or equal to 3, dont do anything
+
+
+  for (var i = 0; i <= this.length; i++) {
+
+  var greaterThan = []
+
+    if (callBackFunction(this[i])) {
+      greaterThan.push(this[i])
+      console.log('true')
+    } else {
+      console.log('false')
+    }
+  }
+  console.log(greaterThan)
+}
