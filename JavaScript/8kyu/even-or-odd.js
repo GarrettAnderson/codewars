85e3a674aeeb2982a11767289569614cf3e27546
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-07-22 18:43:51 -0400

    created two new files for the code katas completed

---
+++ JavaScript/8kyu/even-or-odd.js
@@ -0,0 +1,13 @@
+function even_or_odd(number) {
+    // given a number determine if even or odd
+    
+    // if number can be divided by 2, it is even
+    
+    // if number cannot be divided by 2, it is odd
+    
+    if (number % 2 === 0) {
+      return "Even"
+    } else {
+      return "Odd"
+    }
+  }
