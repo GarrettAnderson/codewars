54b5931fe23819f67f644487bdf954bbd41d9aec
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-07-22 19:16:38 -0400

    created two new files for the code katas completed

---
+++ JavaScript/8kyu/convert-num-to-str.js
@@ -0,0 +1,8 @@
+function numberToString(num) {
+    // Return a string of the number here!
+    
+    let numString = parseInt(num)
+    console.log(numString)
+    console.log(`${numString}`)
+    return `${numString}`
+  }
